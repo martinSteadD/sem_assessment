3e83a27dcf4aa701d5a507e075aacb5f3293db46
Derek Martin-Stead <40690733@live.napier.ac.uk>
2025-10-01 19:35:48 +0100

    updated the comments throughout the project

--- src/main/java/com/napier/sem/populationApp.java
+++ src/main/java/com/napier/sem/populationApp.java
@@ -1,30 +1,40 @@
-package com.napier.sem;
+package com.napier.sem; // Defines the package name for organising classes
 
+// These Imports MongoDB client classes for database interaction
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.MongoCollection;
-import org.bson.Document;
+import org.bson.Document; // BSON document class used to represent MongoDB records
 
 public class populationApp {
 
     public static void main(String[] args)
     {
-        // Connect to MongoDB on local system - we're using port 27000
+        // Connect to MongoDB container named "mongo-dbserver"
+        // Assumes MongoDB is running and accessible on default port 27017
         MongoClient mongoClient = new MongoClient("mongo-dbserver");
-        // Get a database - will create when we use it
+
+        // Access (or create) a database named "mydb"
         MongoDatabase database = mongoClient.getDatabase("mydb");
-        // Get a collection from the database
+
+        // Access (or create) a collection named "test" within "mydb"
         MongoCollection<Document> collection = database.getCollection("test");
-        // Create a document to store
-        Document doc = new Document("name", "Kevin Sim")
-                .append("class", "DevOps")
-                .append("year", "2024")
+
+        // Create a new document with student details
+        Document doc = new Document("name", "Kevin Sim") // Student name
+                .append("class", "DevOps")               // Class name
+                .append("year", "2024")                  // Academic year
                 .append("result", new Document("CW", 95).append("EX", 85));
-        // Add document to collection
+        // Nested document for results: CW (coursework) and EX (exam)
+
+        // Insert the document into the "test" collection
         collection.insertOne(doc);
 
-        // Check document in collection
+        // Retrieve the first document from the collection
         Document myDoc = collection.find().first();
+
+        // Print the document as a JSON string to the console
         System.out.println(myDoc.toJson());
     }
 }
+
